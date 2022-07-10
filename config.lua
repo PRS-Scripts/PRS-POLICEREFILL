9ab537e0ac9907a7b3621cb0dbe2ea22d3f3ceec
Holidayy95 <87913474+Holidayy95@users.noreply.github.com>
2022-07-10 11:22:56 +0100

    another config option

--- config.lua
+++ config.lua
@@ -2,6 +2,7 @@ Config = Config or {}
 
 Config.Coords = vector4(487.13, -995.75, 29.69, 128.89) -- Where the PED will spawn. From basic this is inside the Gabz MRPD - Armory
 
+Config.TakeMyMoney = 'cash' -- or 'bank'
 Config.ChargeOfficer = true
 Config.HealingCost = 100
 Config.StarterCost = 200
